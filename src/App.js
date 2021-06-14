221a0cb7b7dcbda34a363484fd51a42292114c6e
Vikram <vikramtrs@gmail.com>
2021-06-14 02:01:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class Posts extends React.Component {
+  state = {
+    posts: []
+  };
 
-export default App;
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => this.setState({ posts }));
+  }
+
+  renderPosts() {
+    return this.state.posts.map(({ id, title, body }) => 
+      <React.Fragment key={id}>
+        <div className="post-item">{title}</div>
+        <div className="post-item">{body}</div>
+      </React.Fragment>
+    )
+  }
+
+  render() {
+    if (this.state.posts.length === 0) {
+      return <div class="loader"></div>
+    }
+    
+    return (
+      <div className="posts-container">
+        {this.renderPosts()}
+      </div>
+    );
+  }
+}
